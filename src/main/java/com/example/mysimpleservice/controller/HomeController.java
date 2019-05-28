7bbb072706a67a36edd5260214630dc4e00c7d98
DANSAD <daniiar.sadykov@yandex.ru>
2019-05-28 15:15:21 +0600

    Первый коммит

---
+++ src/main/java/com/example/mysimpleservice/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.mysimpleservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(value = "/")
+
+    public String home() {
+
+        return "Eureka Client application";
+
+    }
+
+}
